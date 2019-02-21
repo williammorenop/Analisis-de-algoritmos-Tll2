4093f61407990e9b2407ac50fe37349fbed9e7cd
William Moreno <williammorenoprieto@gmail.com>
2019-02-21 08:51:48 -0500

    Update OrdenamientoContar.java

--- src/OrdenamientoContar.java
+++ src/OrdenamientoContar.java
@@ -1,9 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrdenamientoContar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> numeros = new ArrayList<Integer>();
+		
+		for (int x=0;x<10;x++)
+		numeros.add((int) ((Math.random()*20)+1));
+		
+		ordenaCuentas(1, 20, numeros);
+	}
+	
+	public static void ordenaCuentas(int min,int max, List<Integer> numeros)
+	{
+		List<Integer> contar= new ArrayList<Integer>();
+		for (int i = 0; i < max-min; i++) {
+			contar.add(0);
+		}
+		
+		for (Integer integer : numeros) {
+			contar.set(integer-min, (contar.get(integer))+1);
+		}
+		
+		System.out.println(numeros);
+		System.out.println(contar);
+		
+		
+		
 	}
 
 }
